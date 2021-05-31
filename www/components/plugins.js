463bcb54208feb3cef6a24fecd0b9c098b46d699
camisnet <felixcamily6@gmail.com>
2021-06-01 08:16:42 +0900

    leitor

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,42 @@
 // This is a JavaScript file
 
+$(document).on ("click","#camera",function(){
+  navigator.camera.getPicture(onSuccess, onFail, {
+     quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    correctOrientation: true,
+    saveToPhotoAlbum: true
+});
+function onSuccess(imageURI) {
+    var image = document.getElementById('imagem');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+});
+
+$(document).on("click","#codigo",function(){
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+         navigator.notification.alert("Código lido com sucesso!")
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: true // iOS and Android
+      }
+   );
+});
